7448dcd0c6b56c128e4a28f4999d0967e3a0e385
mammad.askarov <mammad.askarov@GC15PC29>
2022-12-09 00:26:33 +0400

    Add project files.

---
+++ Home Assignment 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Program
+{
+    class Demo
+    {
+        public int RemainderFunc(int a, int b)
+        {
+            if (b == 0)
+                throw new Exception("Second number cannot be zero! Cannot divide by zero!");
+            if (a < b)
+                throw new Exception("Number cannot be less than the divisor!");
+            else
+                return (a % b);
+        }
+        public static void Main()
+        {
+            int a, b;
+            int remainder;
+            Console.Write("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Demo d = new Demo();
+            remainder = d.RemainderFunc(a, b); ;
+            Console.WriteLine("Remainder is: {0}", remainder);
+        }
+    }
+}
